1682c7fb9c4f0fb87833d7d3674946c3e4b47a00
Raymond Tran <traymondbiz@gmail.com>
2020-12-13 17:03:07 -0700

    Implement formatter class

--- src/main/java/AverageFormatter.java
+++ src/main/java/AverageFormatter.java
@@ -1,6 +1,12 @@
 public class AverageFormatter {
 
+    private static final String FORMATTED_TEXT = "The course average is ";
+
     public static String getOutputStatement(double average){
-        return "Bad answer.";
+        return FORMATTED_TEXT + String.format("%.1f", average);
+    }
+
+    public static String getOutputStatement(int average){
+        return FORMATTED_TEXT + average;
     }
 }
--- src/test/java/AverageFormatterTest.java
+++ src/test/java/AverageFormatterTest.java
@@ -29,9 +29,16 @@ public class AverageFormatterTest {
 
 
     @Test
-    public void getOutputStatement_givenMultipleDecimalValues_roundsToOneDecimal() {
+    public void getOutputStatement_givenMultipleDecimalValuesAboveFive_roundsToOneDecimalUp() {
         String expectedResult = "The course average is 73.7";
         String actualResult = AverageFormatter.getOutputStatement(73.666666);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void getOutputStatement_givenMultipleDecimalValuesBelowFive_roundsToOneDecimalDown() {
+        String expectedResult = "The course average is 73.6";
+        String actualResult = AverageFormatter.getOutputStatement(73.64444);
         assertEquals(expectedResult, actualResult);
     }
 }
